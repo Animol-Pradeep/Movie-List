f345fc5587b58b8829b6796666aa29d901a0ad09
Animol Pradeep <animol.pradeep@trenser.com>
2024-07-09 14:36:25 +0530

    Inital App

---
+++ MovieList/MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Animol on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
